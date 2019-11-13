9d79bc8d9a0646c9b2b99b8dd0f852026aeafafb
zzq-00 <1508392680@qq.com>
2019-11-13 19:51:34 +0800

    首页左侧导航和头部

---
+++ topline/src/components/layout-header/layout-header.vue
@@ -0,0 +1,49 @@
+<template>
+    <!-- 头部组件 -->
+    <el-row align='middle' type='flex' justify='space-between'>
+        <el-col :span='6' class="left">
+            <i class="el-icon-s-fold"></i>
+            <span>江苏传智博客</span>
+        </el-col>
+        <el-col :span='4' class="right">
+            <!-- 头像 -->
+            <img src="../../img/zly.jpg" alt="" style="width:30px;height:30px">
+            <!-- 下拉菜单 -->
+            <el-dropdown trigger="click">
+                <span>赵丽颖</span>
+                <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item>账户信息</el-dropdown-item>
+                    <el-dropdown-item>git地址</el-dropdown-item>
+                    <el-dropdown-item>退出</el-dropdown-item>
+                </el-dropdown-menu>
+            </el-dropdown>
+        </el-col>
+    </el-row>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='less' scoped>
+.left{
+    display: flex;
+    align-items: center;
+    i {
+        font-size: 24px;
+    }
+    span{
+        margin-left: 4px;
+    }
+}
+.right {
+    display: flex;
+    align-items: center;
+    img {
+        border-radius: 50%;
+        margin-right:10px;
+    }
+}
+</style>
--- topline/src/views/index/index.vue
+++ topline/src/views/index/index.vue
@@ -1,13 +1,72 @@
 <template>
-    <div>主页</div>
+    <el-container>
+        <!-- 左右布局 -->
+        <el-aside class="left" style="width:220px">
+            <div class="menu-title">
+                <img src="../../img/logo_admin.png" alt="">
+            </div>
+            <!-- 菜单 -->
+            <el-menu
+                style="width:221px"
+                background-color="#353b4e"
+                text-color="#adafb5"
+                active-text-color="#ffd04b"
+            >
+                <!-- 一级菜单 -->
+                <el-menu-item>首页</el-menu-item>
+                <!-- 二级菜单 -->
+                <el-submenu>
+                    <template slot="title">内容管理</template>
+                    <el-menu-item>发布文章</el-menu-item>
+                    <el-menu-item>内容列表</el-menu-item>
+                    <el-menu-item>评论列表</el-menu-item>
+                    <el-menu-item>素材管理</el-menu-item>
+                </el-submenu>
+                <!-- 二级菜单 -->
+                <el-submenu>
+                    <template slot="title">粉丝管理</template>
+                    <el-menu-item>图文数据</el-menu-item>
+                    <el-menu-item>粉丝概况</el-menu-item>
+                    <el-menu-item>粉丝画像</el-menu-item>
+                    <el-menu-item>粉丝列表</el-menu-item>
+                </el-submenu>
+                <el-menu-item>账户信息</el-menu-item>
+            </el-menu>
+        </el-aside>
+        <el-container>
+            <el-header>
+                <layout-header></layout-header>
+            </el-header>
+            <!-- 中间容器--二级路由 -->
+            <el-main>
+                <router-view></router-view>
+            </el-main>
+        </el-container>
+    </el-container>
 </template>
 
 <script>
+import layoutHeader from '../../components/layout-header/layout-header.vue'
 export default {
-
+  components: {
+    'layout-header': layoutHeader
+  }
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
+    .left{
+        width:220px;
+        height: 100vh;
+        background-color: #353b4e;
+        overflow: hidden;
+        .menu-title{
+            padding: 15px 0;
+            text-align: center;
+            background-color:#2e2f32;
+            img{
+                height: 30px;
+            }
+        }
+    }
 </style>
